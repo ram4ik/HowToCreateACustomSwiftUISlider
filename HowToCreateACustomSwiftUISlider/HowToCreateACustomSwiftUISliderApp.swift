c1760826d75083d139431d900f1e414375054619
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2020-08-05 10:50:39 +0300

    Initial Commit

---
+++ HowToCreateACustomSwiftUISlider/HowToCreateACustomSwiftUISliderApp.swift
@@ -0,0 +1,17 @@
+//
+//  HowToCreateACustomSwiftUISliderApp.swift
+//  HowToCreateACustomSwiftUISlider
+//
+//  Created by ramil on 05.08.2020.
+//
+
+import SwiftUI
+
+@main
+struct HowToCreateACustomSwiftUISliderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
